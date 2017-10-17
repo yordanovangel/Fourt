526a0bcd912b4487c4dbb79db08c9aaa4eb8d603
Angel Yordanov <AYordanov@lirex.com>
2017-10-17 11:13:56 +0300

    home, person,...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import { PageNotFoundComponent } from './pege-not-found/page-not-found.component';
+import {HeaderComponent} from './header/header.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { FindPersonComponent } from './features/find-person/find-person.component';
+import { PersonInfoComponent } from './features/person-info/person-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PageNotFoundComponent,
+    HomePageComponent,
+    FindPersonComponent,
+    PersonInfoComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/features/find-person/find-person.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-find-person',
+  templateUrl: './find-person.component.html',
+  styleUrls: ['./find-person.component.css']
+})
+export class FindPersonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
